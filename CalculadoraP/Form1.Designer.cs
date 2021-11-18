f1131523e5f8c3f2d9072e163c40b1af356f275a
Liseth Hernandez <Liseth1.Liseth2@outlook.com>
2021-11-18 02:13:05 -0600

    first commit

---
+++ CalculadoraP/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace CalculadoraP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.Suma = new System.Windows.Forms.Button();
+            this.Borrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Primer numero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Segundo numero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(168, 86);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(158, 20);
+            this.txtNum1.TabIndex = 4;
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(168, 182);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(158, 20);
+            this.txtRes.TabIndex = 5;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(168, 132);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(158, 20);
+            this.txtNum2.TabIndex = 6;
+            // 
+            // Suma
+            // 
+            this.Suma.Location = new System.Drawing.Point(23, 240);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(75, 23);
+            this.Suma.TabIndex = 7;
+            this.Suma.Text = "Suma";
+            this.Suma.UseVisualStyleBackColor = true;
+            this.Suma.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Borrar
+            // 
+            this.Borrar.Location = new System.Drawing.Point(251, 240);
+            this.Borrar.Name = "Borrar";
+            this.Borrar.Size = new System.Drawing.Size(75, 23);
+            this.Borrar.TabIndex = 8;
+            this.Borrar.Text = "Borrar";
+            this.Borrar.UseVisualStyleBackColor = true;
+            this.Borrar.Click += new System.EventHandler(this.Borrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.Borrar);
+            this.Controls.Add(this.Suma);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button Suma;
+        private System.Windows.Forms.Button Borrar;
+    }
+}
+
---
+++ CalculadoraP/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, res;
+            a = int.Parse(txtNum1.Text);
+            b = int.Parse(txtNum2.Text); 
+            res = a + b;
+            txtRes.Text = res.ToString();
+        }
+
+        private void Borrar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            txtRes.Text = "";
+        }
+          
+    }
+}
